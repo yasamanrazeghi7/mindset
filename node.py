d843af45a51b1a127c15b7d48686b3b8f14c71b8
yasamanrazeghi7 <razeghiyasaman@yahoo.com>
2017-06-28 12:36:03 +0430

    Merge

--- node.py
+++ node.py
@@ -4,11 +4,12 @@ class Node(object):
         self.actions_dict = {}
         # for index, action in enumerate(self.actions):
         #     self.add_action(action, self.rewards[index], self.next_nodes[index])
+
     def add_action(self, action, reward, next_node):
         self.actions_dict[action] = (reward, next_node)
 
     def get_reward(self, action):
         return self.actions_dict[action]
 
-if __name__ == "__main__":
-    print("Salam imported node")
+    def get_name(self):
+        return self.name
---
+++ trial.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class State(Enum):
+    NOT_STARTED = 1
+    IN_THE_MIDDLE = 2
+    FINISHED = 3
+
+
+class Trial(object):
+    def __init__(self, moves_number, start_state):
+        self.moves_number = moves_number
+        self.start_state = start_state
+        self.current_state = start_state
+        self.total_reward = 0
+        self.remaining_moves = moves_number
+        self.state = State.NOT_STARTED
+
+    def get_current_node(self):
+        return self.current_node
+
+    def get_reward(self):
+        return self.total_reward
+
+    def get_state(self):
+        return self.state
+
+    def do_action(self, action):
+        if self.state == State.FINISHED:
+            print ("the trial is finished")
+            return
+        self.state = State.IN_THE_MIDDLE
+        [reward, n_node] = self.current_state.get_reward( action)
+        self.total_reward += reward
+        self.current_state = n_node
+        self.remaining_moves = self.remaining_moves - 1;
+        if self.remaining_moves == 0:
+            self.state = State.FINISHED
+
+    def do_many_actions(self, actions):
+        for a in actions:
+            self.do_action(a)
