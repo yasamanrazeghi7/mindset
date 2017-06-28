020b3af698795ec15256c52bb626e7e9021de0cf
yasamanrazeghi7 <razeghiyasaman@yahoo.com>
2017-06-28 10:52:40 +0430

    node added

---
+++ node.py
@@ -0,0 +1,14 @@
+class Node(object):
+    def __init__(self, name):
+        self.name = name
+        self.actions_dict = {}
+        # for index, action in enumerate(self.actions):
+        #     self.add_action(action, self.rewards[index], self.next_nodes[index])
+    def add_action(self, action, reward, next_node):
+        self.actions_dict[action] = (reward, next_node)
+
+    def get_reward(self, action):
+        return self.actions_dict[action]
+
+if __name__ == "__main__":
+    print("Salam imported node")
